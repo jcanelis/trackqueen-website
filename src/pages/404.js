7223c31faf7539bf7c31a250c55b56cc251f31c7
John Canelis <john@withroam.com>
2024-10-04 19:59:36 -0400

    added title to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import * as React from "react"
 
 const NotFoundPage = () => (
   <main style={{ padding: 80 }}>
+    <title>TrackQueen – ssage not found</title>
     <h1 style={{ textAlign: "center" }}>Page not found</h1>
   </main>
 )
